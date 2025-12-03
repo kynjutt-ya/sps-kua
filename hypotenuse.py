7975612e4ca65d274cd7f81e69859678a9e2edc4
Julia Kugut <kugut.yulya@mail.ru>
2025-12-03 15:25:57 +0300

    calculator

--- hypotenuse.py
+++ hypotenuse.py
@@ -1,9 +1,15 @@
 import math
 
+def get_area(a, b):
+    return a * b
+
 def get_hypotenuse(a, b):
     return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    print(get_hypotenuse(a,b))
+    print("Введите a:")
+    a = int(input())
+    print("Введите b:")
+    b = int(input())
+    print("c =", get_hypotenuse(a,b))
+    print("S =", get_area(a,b))
